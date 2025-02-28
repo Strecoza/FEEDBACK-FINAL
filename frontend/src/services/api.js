e6ab0aae65fb0ecb2fad29451dee4d5d5d5c8b0e
TatianaPri <87344989+Strecoza@users.noreply.github.com>
2025-02-28 12:01:16 -0800

    settings for render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "https://feedback-frontend-fib0.onrender.com";
+const API_URL = process.env.REACT_APP_API_URL || "https://feedback-app-ycvv.onrender.com";
 
 const api = axios.create({
     baseURL: API_URL,
